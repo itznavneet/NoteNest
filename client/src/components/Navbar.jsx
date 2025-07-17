e7ecc275958a1ee786bbcf3ada5a590d6e17daeb
Navneet Jaiswal <navypc50@gmail.com>
2025-07-17 15:07:00 +0530

    Update Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,23 +1,15 @@
 import { Link, useNavigate } from "react-router-dom";
-import { useEffect, useState } from "react";
 import "./Navbar.css";
-  import {toast } from 'react-toastify';
-  import { useAuth } from "../context/AuthContext";
-
+import { toast } from "react-toastify";
+import { useAuth } from "../context/AuthContext";
 
 export default function Navbar() {
-  const {logout, user}= useAuth()
+  const { logout, user } = useAuth();  // get user directly from context
   const navigate = useNavigate();
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    setIsLoggedIn(!!user); // true if user exists
-  }, [localStorage.getItem("event-noter-user")]);
 
   const handleLogout = () => {
     logout();
-    toast.info("Logged out")
-    setIsLoggedIn(false);
+    toast.info("Logged out");
     navigate("/login");
   };
 
@@ -26,7 +18,7 @@ export default function Navbar() {
       <div className="navbar-logo">NoteNest</div>
 
       <div className="navbar-right">
-        {!isLoggedIn ? (
+        {!user ? (
           <>
             <Link to="/login" className="nav-btn">Login</Link>
             <Link to="/register" className="nav-btn secondary">Register (new user?)</Link>
